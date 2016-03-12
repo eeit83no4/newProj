1377940662520707bb42a1d1f78d1910c138ea1a
Colin <colingood0@gmail.com>
2016-03-12 10:16:15 +0800

    showPicAction新增如果沒有圖片就使用預設圖片

--- projHY20160201/src/module/controller/userOrder/showPicAction.java
+++ projHY20160201/src/module/controller/userOrder/showPicAction.java
@@ -8,6 +8,7 @@ import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.ServletResponseAware;
 
 import com.opensymphony.xwork2.Action;
@@ -43,7 +44,28 @@ public class showPicAction extends ActionSupport implements ServletResponseAware
 		byte[] imgByte=_12dao.findById(itemno).getPic();
 		if(imgByte==null){//如果圖片不存在
 						
-			//......?
+			//預設圖片			
+			String anonymous=ServletActionContext.getServletContext().getRealPath("/images/anonymous.png");
+			File file=new File(anonymous);			
+			byte[] photo=new byte[(int)file.length()];	
+			
+			try {
+				FileInputStream fileInputStream=new FileInputStream(file);
+				OutputStream os =response.getOutputStream();
+				//使用緩衝區
+				BufferedInputStream bufferedInputStream=new BufferedInputStream(fileInputStream);
+				BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(os);
+				while(bufferedInputStream.read(photo)!=-1){
+					bufferedOutputStream.write(photo);
+				}
+				// 將緩衝區中的資料全部寫出
+				bufferedOutputStream.flush();
+				// 關閉串流
+				bufferedInputStream.close();
+				bufferedOutputStream.close();
+			} catch (Exception e) {
+				System.out.println("圖片輸出失敗："+e.toString());				
+			}			
 			
 		}else{//如果圖片存在
 			String imgString=new String(imgByte);

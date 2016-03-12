e4ce6fcbbf132d7268838168fb6797c323a479f6
Colin <colingood0@gmail.com>
2016-03-12 10:29:18 +0800

    showPicAction修正

--- projHY20160201/src/module/controller/userOrder/showPicAction.java
+++ projHY20160201/src/module/controller/userOrder/showPicAction.java
@@ -42,60 +42,35 @@ public class showPicAction extends ActionSupport implements ServletResponseAware
 		_12_ItemDAO _12dao=new _12_ItemDAO();
 		//取得圖片來源，從資料庫
 		byte[] imgByte=_12dao.findById(itemno).getPic();
-		if(imgByte==null){//如果圖片不存在
-						
+		String imgString=null;
+		if(imgByte==null){//如果圖片不存在						
 			//預設圖片			
-			String anonymous=ServletActionContext.getServletContext().getRealPath("/images/anonymous.png");
-			File file=new File(anonymous);			
-			byte[] photo=new byte[(int)file.length()];	
-			
-			try {
-				FileInputStream fileInputStream=new FileInputStream(file);
-				OutputStream os =response.getOutputStream();
-				//使用緩衝區
-				BufferedInputStream bufferedInputStream=new BufferedInputStream(fileInputStream);
-				BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(os);
-				while(bufferedInputStream.read(photo)!=-1){
-					bufferedOutputStream.write(photo);
-				}
-				// 將緩衝區中的資料全部寫出
-				bufferedOutputStream.flush();
-				// 關閉串流
-				bufferedInputStream.close();
-				bufferedOutputStream.close();
-			} catch (Exception e) {
-				System.out.println("圖片輸出失敗："+e.toString());				
-			}			
-			
+			imgString=ServletActionContext.getServletContext().getRealPath("/images/anonymous.png");
 		}else{//如果圖片存在
-			String imgString=new String(imgByte);
-			File file=new File(imgString);
-			
-			byte[] photo=new byte[(int)file.length()];	
-			
-			try {
-				FileInputStream fileInputStream=new FileInputStream(file);
-				OutputStream os =response.getOutputStream();
-				//使用緩衝區
-				BufferedInputStream bufferedInputStream=new BufferedInputStream(fileInputStream);
-				BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(os);
-				while(bufferedInputStream.read(photo)!=-1){
-					bufferedOutputStream.write(photo);
-				}
-				// 將緩衝區中的資料全部寫出
-				bufferedOutputStream.flush();
-				// 關閉串流
-				bufferedInputStream.close();
-				bufferedOutputStream.close();
-			} catch (Exception e) {
-				System.out.println("圖片輸出失敗："+e.toString());				
-			}			
+			imgString=new String(imgByte);						
 		}
 		
-		
-
-		
-		
+		if(imgString!=null){
+			File file=new File(imgString);		
+			byte[] photo=new byte[(int)file.length()];		
+			try {
+				FileInputStream fileInputStream=new FileInputStream(file);
+				OutputStream os =response.getOutputStream();
+				//使用緩衝區
+				BufferedInputStream bufferedInputStream=new BufferedInputStream(fileInputStream);
+				BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(os);
+				while(bufferedInputStream.read(photo)!=-1){
+					bufferedOutputStream.write(photo);
+				}
+				// 將緩衝區中的資料全部寫出
+				bufferedOutputStream.flush();
+				// 關閉串流
+				bufferedInputStream.close();
+				bufferedOutputStream.close();
+			} catch (Exception e) {
+				System.out.println("圖片輸出失敗："+e.toString());				
+			}
+		}		
 		return Action.NONE;
 	}
 	
